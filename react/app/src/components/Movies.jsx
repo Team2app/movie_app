b5feca09f3346c154548d1e0d7c297ec1b13ab1c
Team2app <cheetirala.venkat@incedoinc.com>
2023-02-20 20:39:50 +0530

    first

---
+++ react/app/src/components/Movies.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Movies() {
+  return (
+    <div>
+      <h1>Movies List</h1>
+    </div>
+  )
+}
---
+++ react/app/src/components/NavBar.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { useState } from 'react';
+import { HiSearch } from 'react-icons/hi';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import '../styles/Navbar.css'
+import Home from './Home';
+import Movies from './Movies';
+
+export default function NavBar() {
+  const [toggle, setToggle] = useState(true);
+  return (
+    <Fragment>
+      <nav className={toggle ? '' : 'navBarColor'} >
+        <div className='nav-options'>
+          <h1 id={toggle ? '' : 'heading'}> IncedoFlix </h1>
+          <NavLink to="/">
+            <span id={toggle ? 'Movies' : 'MoviesLight'} >Home</span>
+          </NavLink>
+          <NavLink to="/movies">
+            <span id={toggle ? 'Movies' : 'MoviesLight'} >Movies</span>
+          </NavLink>
+        </div>
+        <div className='input-group'>
+          <input type="text" placeholder='Search the city' />
+          <HiSearch fontSize={21} color="green" id='search' />
+          <div id='Color-switcher' onClick={() => setToggle(!toggle)}>
+            <div id={toggle ? 'Color-switcher-mover' : 'Color-switcher-moved'}>
+
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/movies" element={<Movies />} ></Route>
+      </Routes>
+    </Fragment>
+  )
+}
