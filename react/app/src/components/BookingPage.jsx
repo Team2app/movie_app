cc1b914b982cc5cbdf3ae08c9291442c5595bc5d
mani-venkat <manivenkat4729@gmail.com>
2023-02-23 00:56:55 +0530

    seats correction

--- react/app/src/components/BookingPage.jsx
+++ react/app/src/components/BookingPage.jsx
@@ -8,25 +8,30 @@ export default function BookingPage() {
     const [count, setCount] = useState(0);
     const [total, setTotal] = useState(0);
     const [price, setPrice] = useState(1);
-    const handleClickA = () =>{
-        setCount(selectedSeats.length + 1);
-        setTotal((selectedSeats.length + 1)*price);
+    const [c, setC] = useState(0);
+    const handleClickA = (val) =>{
+        if(!val){
+            setCount(selectedSeats.length-c+1);
+            setTotal((selectedSeats.length-c+1)*price);
+        }
+        else{
+            setC(c+1);
+        }
     }
-    const handleClickS = () =>{
-        setCount(selectedSeats.length - 1);
-        setTotal((selectedSeats.length - 1)*price);
+    const handleClickS = (val) =>{
+        if(!val){
+            setCount(selectedSeats.length - c - 1);
+            setTotal((selectedSeats.length - c - 1)*price);
+        }
+        else{
+            setC(c-1);
+        }
     }
     return (
         <div className='body'>
             <div className="single-container">
-                <label>Select a movie:</label>
-                <select id="single" onClick={(e)=>{ setPrice(e.target.value) }}>
-                    <option value="0" >Select Movie</option>
-                    <option value="220" >Godzilla vs Kong</option>
-                    <option value="330" >Radhe</option>
-                    <option value="440" >RRR</option>
-                    <option value="110" >F9</option>
-                </select>
+                <label>Select the date:</label>
+                <input type="date" id="single" name="birthday" />
             </div>
             <ul className='showcase'>
                 <li>
@@ -48,7 +53,7 @@ export default function BookingPage() {
                     rows.map((row)=>{
                         return (<div className='row' key={row.id}>
                             {seats.map((seat)=>{
-                                return (<div className={selectedSeats.includes(row.value+seat.id) && !selseats.includes(row.value+seat.id) ?'seat selected' : selseats.includes(row.value+seat.id) ? 'seat sold' : seat.seat} onClick={(e)=>{ selectedSeats.includes(row.value+seat.id) ?setSelectedSeats(selectedSeats.filter(s=>!s.includes(row.value+seat.id))) :setSelectedSeats([...selectedSeats,row.value+seat.id]); selectedSeats.includes(row.value+seat.id) ? handleClickS() : handleClickA(); console.log(e)}} key={row.value + seat.id}></div>)
+                                return (<div className={selectedSeats.includes(row.value+seat.id) && !selseats.includes(row.value+seat.id) ?'seat selected' : selseats.includes(row.value+seat.id) ? 'seat sold' : seat.seat} onClick={(e)=>{ selectedSeats.includes(row.value+seat.id) ?setSelectedSeats(selectedSeats.filter(s=>!s.includes(row.value+seat.id))) :setSelectedSeats([...selectedSeats,row.value+seat.id]);  selectedSeats.includes(row.value+seat.id) ? handleClickS(selseats.includes(row.value+seat.id)) : handleClickA(selseats.includes(row.value+seat.id)) }} key={row.value + seat.id}></div>)
                             })}
                         </div>)
                     })
